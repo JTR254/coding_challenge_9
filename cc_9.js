57d9e9913a9d5b5cc7c59e9d3eb9d6650039ba2e
JTR254 <jackrindge@gmail.com>
2025-02-23 17:03:36 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -1,4 +1,4 @@
-// Task 1 - Create Employee Class
+// Task 1 - Employee Class
 class Employee { // creates employee class with details
     constructor (name, id, department, salary) {
         this.name = name;
@@ -17,3 +17,22 @@ class Employee { // creates employee class with details
 const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 console.log(emp1.getDetails()); // Logs the method to the console - Output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
 console.log(`Annual Salary: $${emp1.calculateAnnualSalary()}`); // Logs the method to the console - Output: Annual Salary: $60000
+
+// Task 2 - Manager Class with Inheritance
+class Manager extends Employee { // creates Manager class that extends to the Employee class
+    constructor (name, id, department, salary, teamsize) {
+        super (name, id, department, salary);
+        this.teamsize = teamsize // defines teamsize
+        
+    }
+    getDetails() { // creates method that adds team size to the class details
+        return `Manager: ${this.name}, Employee ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamsize}`
+    }
+    calculateBonus() { // creates method that returns 10% of the Manager's annual salary
+        return this.salary * 12 * .10
+    }
+};
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails()); // logs the method to the console - Output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+
+console.log(`Manager Bonus: $${mgr1.calculateBonus()}`); // logs the method to the console - Output: 9600
